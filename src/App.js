52bd20ee2a5aad4442dea06d21447e74b9ac9e87
Jack Morrissey <j.morrissey.16@gmail.com>
2018-12-16 22:48:55 -0600

    setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home"
+import Portfolio from "./pages/Portfolio/Portfolio"
+import Contact from "./pages/Contact/Contact"
+import NavBar from "./components/NavBar/NavBar"
+import NotFound from "./pages/NotFound/NotFound"
+
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path ="/" component = {Home} />
+        <Route exact path = "/portfolio" component = {Portfolio} />
+        <Route exact path = "/contact" component = {Contact} />
+        <Route component = {NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
