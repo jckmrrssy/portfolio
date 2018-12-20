6fa1d832e18deb1c3205a72cbe2db2437d075da8
Jack Morrissey <j.morrissey.16@gmail.com>
2018-12-20 10:17:10 -0600

    Nav work

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,14 @@
+import React from "react";
+import { Navbar, NavItem } from "react-materialize";
+
+const NavBar = () => (
+    
+    <Navbar brand="Jack Morrissey">
+        <NavItem href = "#">Portfolio</NavItem>
+        <NavItem href = "#">Contact</NavItem> 
+    </Navbar>
+)
+
 
 
 export default NavBar;
