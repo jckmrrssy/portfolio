990082458d7883a9ee032c4edc175fbb96255f64
Jack Morrissey <j.morrissey.16@gmail.com>
2018-12-20 11:36:04 -0600

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
     render() {
         
         return (
-           
+                
                 <Navbar brand='Jack Morrissey' right>
                     <NavItem href = "#" className ="nav-link">Portfolio</NavItem>
                     <NavItem href = "#" className = "nav-link">Contact</NavItem> 
