eb46e6486fbc291572fdd1eba7d031b8068b3803
Jack Morrissey <j.morrissey.16@gmail.com>
2019-01-16 09:06:14 -0600

    navbar/jumbotron

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,21 +8,10 @@ export default class NavBar extends Component {
     // <a href="#" className="hamburgerGuy"><FontAwesomeIcon icon={faBars} /></a>
     render() {
         return (  
-                // <Navbar brand='[ jem ]' className="navbar transparent" href="#aboutMeSection" right>
-                //     <NavItem href = "#portfolioSection" className ="nav-link">Portfolio</NavItem>
-                //     <NavItem href = "#contactSection" className = "nav-link">Contact</NavItem> 
-                // </Navbar>  
-                <nav>
-                    <div class="nav-wrapper">
-                    <a href="#" className="brand-logo">[ jem ]</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">JavaScript</a></li>
-                    </ul>
-                    </div>
-              </nav>
-                    
+                <Navbar brand='[ jem ]' className="navbar transparent" href="#aboutMeSection" right>
+                    <NavItem href = "#portfolioSection" className ="nav-link">Portfolio</NavItem>
+                    <NavItem href = "#contactSection" className = "nav-link">Contact</NavItem> 
+                </Navbar>         
             )
         };
     };
