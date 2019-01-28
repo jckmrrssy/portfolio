656d7c1330cf9bb55540447b1cd63e2b47f4eb42
Jack Morrissey <j.morrissey.16@gmail.com>
2019-01-27 21:23:21 -0600

    carousel work

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ScrollableAnchor from "react-scrollable-anchor";
-import { Col, Row } from "react-materialize";
+import { Col, Row, Carousel } from "react-materialize";
+import "./AboutMe.css"
 
 export default class AboutMe extends Component {
   render() {
@@ -27,6 +28,20 @@ export default class AboutMe extends Component {
                                 <p className="center-align">I'm a Full-Stack Web Developer who can design and build refined applications from mockup to rollout with a full database and back-end. My passion for development is rooted in my love for creative problem solving and my desire to make everyday life easier for app users. I'm particularly savvy working with front-end technologies and am always eager to learn new skills.</p>
                                 <p className="center-align">I have two years of experience in the financial services industry and just completed the University of Minnesota’s Full-Stack Developer program. I'm currently searching for a full-time junior developer or software engineering opportunity. If you are in need of a developer, check out some of my portfolio highlights below and feel free to contact me with any questions.</p>
                             </Col>
+                        </Row>
+
+                        <Row>
+                            <Col s={12} className="skillsCarousel">
+                                <Carousel options={{padding: 100, duration: 500, indicators: true }} images={[
+                                        '../../images/HTML5_Logo.svg',
+                                        '../../images/CSS3_Logo.png',
+                                        '../../images/JS6.png',
+                                        '../../images/Bootstrap_Logo.png',
+                                        '../../images/Materialize_Logo.png',
+                                        '../../images/jQuery_Logo.png'
+                                        ]}
+                                    />
+                                </Col>
                         </Row>
                     </div>
                 </ScrollableAnchor>
