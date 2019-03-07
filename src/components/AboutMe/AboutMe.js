9d9e2b9ba6980cb598c87bb06e0efc5699140483
Jack Morrissey <j.morrissey.16@gmail.com>
2019-03-06 19:27:05 -0600

    changing about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,72 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ScrollableAnchor from "react-scrollable-anchor";
 import { Col, Row } from "react-materialize";
-import SkillSlider from "../SkillSlider/SkillSlider"
-import "./AboutMe.css"
-
+import "./AboutMe.css";
 
 export default class AboutMe extends Component {
+	render() {
+		return (
+			<div>
+				{/* About me */}
+				<div className="section white">
+					<ScrollableAnchor id={"aboutMeSection"}>
+						<div className="row-container aboutMeRow">
+							<Row>
+								<Col s={12} l={4} className="placeHolderGuy" />
+								<Col s={12} l={8} className="center-align">
+									<h3 className="center-align">About me</h3>
+								</Col>
+							</Row>
 
-    render() {
-    return (
-      <div>
-            {/* About me */}
-            <div className="section white">
-                <ScrollableAnchor id={"aboutMeSection"} >
-                    <div className="row-container aboutMeRow">
-                        <Row>
-                            <Col s={12} l={4} className="placeHolderGuy"></Col>
-                            <Col s={12} l={8} className="center-align">
-                                <h3 className="center-align">About me</h3>
-                            </Col>
-                        </Row>
-                        
-                        <Row>
-                            {/* about me pic */}
-                            <Col s={12} l={4} className="picGuy center-align">
-                                <img alt="a pic of jack" className="imgGuy responsive-img" src="../../images/mePic.jpg" />
-                            </Col>
-                            {/* about me text */}
-                            <Col s={12} l={8} className="aboutText">
-                                <p className="center-align">I'm a Full-Stack Web Developer who can design and build refined applications from mockup to rollout with a full database and back-end. My passion for development is rooted in my love for creative problem solving and my desire to make everyday life easier for app users. I'm particularly savvy working with front-end technologies and am always eager to learn new skills.</p>
-                                <p className="center-align">I have two years of experience in the financial services industry and just completed the University of Minnesota’s Full-Stack Developer program. I'm currently searching for a full-time junior developer or software engineering opportunity. If you are in need of a developer, check out some of my portfolio highlights below and feel free to contact me with any questions.</p>
-                                
-                            </Col>
-                        </Row>
-
-                        <Row>
-                            <Col s={12} className="skillsCarousel">
-
-                            <SkillSlider />
-
-
-                                {/* <Carousel options={{ padding: 125, duration: 500, indicators: true }} images={[
-                                        '../../images/htmlLogo.png',
-                                        '../../images/cssLogo.png',
-                                        '../../images/JS6.png',
-                                        '../../images/ReactJSLogo.png',
-                                        '../../images/nodeLogo.png',
-                                        '../../images/npmLogo.png',
-                                        '../../images/mongoDBLogo.jpg',
-                                        '../../images/mongooseLogo.png',
-                                        '../../images/mySQLLogo.jpg',
-                                        '../../images/sequelizeLogo.png',
-                                        '../../images/expressJSLogo.png',
-                                        '../../images/bootstrapLogo.jpg',
-                                        '../../images/materializeLogo.png',
-                                        '../../images/jQuery_Logo.png',
-                                        '../../images/ajaxLogo.svg',
-                                        '../../images/gitLogo.svg',
-                                        '../../images/herokuLogo.png',
-                                        '../../images/VSCodeLogo.png'
-                                        ]}
-                                    /> */}
-                                </Col>
-                        </Row>
-                    </div>
-                </ScrollableAnchor>
-            </div>
-      </div>
-    )
-  }
+							<Row>
+								{/* about me pic */}
+								<Col s={12} l={4} className="picGuy center-align">
+									<img
+										alt="a pic of jack"
+										className="imgGuy responsive-img"
+										src="../../images/mePic.jpg"
+									/>
+								</Col>
+								{/* about me text */}
+								<Col s={12} l={8} className="aboutText">
+									<p className="center-align">
+										I'm a Full-Stack Web Developer who can design and build
+										refined applications from mockup to rollout. My passion for
+										development is rooted in my love for creative problem
+										solving and my desire to make everyday life easier for app
+										users.
+									</p>
+									<p className="center-align">
+										I'm particularly savvy working with front-end technologies
+										and am always eager to learn new skills. University of
+										Minnesota developer program graduate.
+									</p>
+								</Col>
+							</Row>
+						</div>
+					</ScrollableAnchor>
+				</div>
+			</div>
+		);
+	}
 }
